187f3ba04be500795e9266310e28ed70bed5472e
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-04-02 19:19:47 +0800

    feat: add function to showcase slider sizing the entire canvas

--- script.js
+++ script.js
@@ -9,9 +9,32 @@ const gval = gcontainer.querySelectorAll('span.gval');
 /* canvas */
 const canvas = document.querySelector('.canvas');
 
-gslider.oninput = function(e) {
+let size = gslider.getAttribute('value');
+displayGrid(size);
+
+gslider.oninput = function (e) {
   gval.forEach(val => {
     val.textContent = e.target.value;
+  });
+  size = e.target.value;
+  clearGrid();
+  displayGrid(size);
+}
+
+function displayGrid(size) {
+  canvas.style.setProperty('--grid-rows', size);
+  canvas.style.setProperty('--grid-cols', size);
+
+  for (let index = 0; index < (size * size); index++) {
+    let cell = document.createElement('div');
+    canvas.appendChild(cell).className = "grid-cell";
+  };
+};
+
+function clearGrid() {
+  let display = canvas.querySelectorAll('.grid-cell');
+  display.forEach(box => {
+    box.remove();
   });
 }
 
